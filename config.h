240c1fecd8ef4f71aa4b69e34114f3bc527a2228
Juan Alberto Raya <22559891+Juanal07@users.noreply.github.com>
2022-01-03 22:19:58 +0100

    auto push

--- config.h
+++ config.h
@@ -77,7 +77,6 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *roficmd[] = { "rofi", "-show", "drun", "-show-icons", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include <X11/XF86keysym.h>
